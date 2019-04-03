2534bdd2e852f323f041406931379099fcd80006
MJ FullStack <mjohnson3@cox.net>
2019-04-03 11:53:30 -0700

    Remake of ERC20 directory for GIT

---
+++ contracts/SampleToken.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.24;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+
+
+contract SampleToken is ERC20Detailed, ERC20 {
+
+    // string public name = "UdacitySampleToken";
+    // string public symbol = "UST";
+    // uint public decimals = 18;
+    // uint public INITIAL_SUPPLY = 10000 * ( 10 ** decimals );
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint _initialSupply) 
+    ERC20Detailed(_name, _symbol, _decimals) public {
+        require(_initialSupply > 0, "INITIAL_SUPPLY has to be greater than 0");
+        _mint(msg.sender, _initialSupply);
+    }
+}
+
+// SampleToken(name, symbol, decimals, INITIAL_SUPPLY);
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const SampleToken = artifacts.require("SampleToken");
+
+module.exports = function(deployer) {
+  // [, arg1, arg2, ... ] To add Constructor Arguments if desired. 
+  // Our's were vars in SampleToken file/contract... DIDN'T COMPILE
+  deployer.deploy(SampleToken, "UdacitySampleToken", "UST", 18, 10000);
+};
